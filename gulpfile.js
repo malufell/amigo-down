587cff1c35f4e67201ed3637a9236ffe408717a2
Malu <malufell@hotmail.com>
2021-04-26 16:26:32 -0300

    adiciona gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,78 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
 const server = require('browser-sync').create();
 const { watch, series } = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+    return gulp.src('./build/**/*')
+        .pipe(ghPages({
+            remoteUrl: 'https://github.com/malufell/amigo-down',
+            branch: 'master'
+        }));
+});
 
 const paths = {
-  scripts: {
-    src: './',
-    dest: './build/'
-  }
+    scripts: {
+        src: './',
+        dest: './build/'
+    }
 };
 
 // Reload Server
 async function reload() {
-  server.reload();
+    server.reload();
 }
 
 // Sass compiler
 async function compileSass() {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'));
 }
 
 // Copy assets after build
 async function copyAssets() {
-  gulp.src(['assets/**/*'])
-    .pipe(gulp.dest(paths.scripts.dest));
+    gulp.src(['assets/**/*'])
+        .pipe(gulp.dest(paths.scripts.dest));
 }
 
 // Build files html and reload server
 async function buildAndReload() {
-  await includeHTML();
-  await copyAssets();
-  reload();
+    await includeHTML();
+    await copyAssets();
+    reload();
 }
 
-async function includeHTML(){
-  return gulp.src([
-    '*.html',
-    '!head.html', // ignore
-    '!header.html', // ignore
-    '!footer.html', // ignore
-    '!script.html' // ignore
-    ])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest(paths.scripts.dest));
+async function includeHTML() {
+    return gulp.src([
+            '*.html',
+            '!head.html', // ignore
+            '!header.html', // ignore
+            '!footer.html', // ignore
+            '!script.html' // ignore
+        ])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest(paths.scripts.dest));
 }
 exports.includeHTML = includeHTML;
 
 exports.default = async function() {
-  // Init serve files from the build folder
-  server.init({
-    server: {
-      baseDir: paths.scripts.dest
-    }
-  });
-  // Build and reload at the first time
-  buildAndReload();
-  // Watch Sass task
-  watch('./sass/**/*.scss',  series(compileSass));
-  // Watch task
-  watch(["*.html","assets/**/*"], series(buildAndReload));
+    // Init serve files from the build folder
+    server.init({
+        server: {
+            baseDir: paths.scripts.dest
+        }
+    });
+    // Build and reload at the first time
+    buildAndReload();
+    // Watch Sass task
+    watch('./sass/**/*.scss', series(compileSass));
+    // Watch task
+    watch(["*.html", "assets/**/*"], series(buildAndReload));
 };
